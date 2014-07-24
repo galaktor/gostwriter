3d14c3403563a81a7db9a7610df3f8a5773b9402
Raphael Estrada <galaktor@gmx.de>
2014-07-24 18:09:12 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#What is it?
+It's a simple virtual keyboard for Go that uses /dev/uinput to inject key events, as if from a real keyboard.
+
+#Why?
+To programatically emulate user keystrokes, i.e. an emulated gamepad or other device that can map to keyboard inputs.
+
+#Limitations
+It currently only does keyboard events, and only the ones I needed so far. Can be extended in the scope of uinput, i.e. mouse events, but I didn't do that yet. Feel free to fork!
+
+#Copyright
+Licensed under the GPL v3. See LICENSE and COPYRIGHT files.
---
+++ virtkey.go
@@ -0,0 +1,24 @@
+/*  Copyright 2014, Raphael Estrada
+    Author email:  <galaktor@gmx.de>
+    Project home:  <https://github.com/galaktor/gostwriter>
+    Licensed under The GPL v3 License (see README and LICENSE files) */
+package gostwriter
+
+type VirtualKeyboard struct {
+	device UinputDevice
+//	keys map[string]Key 
+}
+
+func New(devName string, keys ...Key) (*VirtualKeyboard, error) {
+	dev, err := Create(devName)
+	if err != nil {
+		return err
+	}
+
+	vk := &VirtualKeyboard{}
+	vk.device = dev
+	vk.register()
+	vk.defKeys()
+
+	return vk, nil	
+}
