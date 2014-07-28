3d17f59a7d0e86067f1c65446fd395882f17abc7
Raphael Estrada <galaktor@gmx.de>
2014-07-28 20:28:52 +0100

    add tests for virtkey

---
+++ virtkey_test.go
@@ -0,0 +1,33 @@
+/*  Copyright 2014, Raphael Estrada
+    Author email:  <galaktor@gmx.de>
+    Project home:  <https://github.com/galaktor/gostwriter>
+    Licensed under The GPL v3 License (see README and LICENSE files) */
+package gostwriter
+
+import "testing"
+
+func TestNew_Always_CreatesUinputDevice(t *testing.T) {
+	t.Error("todo")
+}
+
+func TestGet_DefinedKey_ReturnsThatKey(t *testing.T) {
+	t.Error("todo")
+}
+
+func TestGet_UndefinedKey_ReturnsError(t *testing.T) {
+	t.Error("todo")
+}
+
+func TestDestroy_Always_DestroysUinputDevice(t *testing.T) {
+	t.Error("todo")
+}
+
+
+
+
+
+
+
+
+
+
