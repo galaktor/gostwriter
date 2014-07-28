60b4f1803870e9dbf693a87a9ed76f048f62ce04
Raphael Estrada <galaktor@gmx.de>
2014-07-28 20:28:40 +0100

    add push to key

--- key.go
+++ key.go
@@ -2,7 +2,7 @@
     Author email:  <galaktor@gmx.de>
     Project home:  <https://github.com/galaktor/gostwriter>
     Licensed under The GPL v3 License (see README and LICENSE files) */
-package uinput
+package gostwriter
 
 /*
   #include <linux/input.h>
@@ -29,14 +29,32 @@ func newKey(keyCode int, ) (*Key) {
 	
 }
 
+/* press the key then release it in sequence */
+func (k *K) Push() error {
+	err := k.Press()
+	if err != nil {
+		return err
+	}
+
+	err = k.Release()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (k *K) IsPressed() bool {
 	return k.IsPressed
 }
 
 func (k *K) Press() error {
-	return dev.Press(k.keyCode)
+	return k.dev.Press(k.keyCode)
 }
 
+func (k *K) Release() error {
+	return k.dev.Release(k.keyCode)
+}
 
 
 
