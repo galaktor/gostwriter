618ac8cb3499ae287d08f6fa9199e9d5df338657
Raphael Estrada <galaktor@gmx.de>
2014-07-29 11:02:19 +0100

    Update get_keycodes.sh
    add type alias for KeyCode

--- uinput/get_keycodes.sh
+++ uinput/get_keycodes.sh
@@ -9,6 +9,9 @@ echo "  #include <linux/input.h>" >> keycodes.go
 echo "*/" >> keycodes.go
 echo "import \"C\"" >> keycodes.go
 echo "" >> keycodes.go
+echo "" >> keycodes.go
+echo "type KeyCode C.__u16" >> keycodes.go
+echo "" >> keycodes.go
 echo "const(" >> keycodes.go
 curl -s https://raw.githubusercontent.com/torvalds/linux/master/include/uapi/linux/input.h | grep -e KEY_ | awk '{printf("    %-21s = C.%-23s  /* %-5s */\n", $2, $2, $3)}' >> keycodes.go
 echo ")" >> keycodes.go
