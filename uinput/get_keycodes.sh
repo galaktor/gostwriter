f776d092361ed32fe9b3a69565e1837cd041dfe3
Raphael Estrada <galaktor@gmx.de>
2014-07-29 13:31:32 +0100

    Update get_keycodes.sh
    comment ALL_CODES stuff from auto-gen of keycodes. better to create a go file template, and sed-replace the inside of const(...) with the KEY stuff instead.

--- uinput/get_keycodes.sh
+++ uinput/get_keycodes.sh
@@ -24,9 +24,9 @@ echo "" >> keycodes.go
 echo "const(" >> keycodes.go
 cat $inputh | grep -e KEY_ | awk '{printf("    %-21s = KeyCode(C.%-23s)  /* %-5s */\n", $2, $2, $3)}' >> keycodes.go
 echo ")" >> keycodes.go
-echo "" >> keycodes.go
-echo "var ALL_CODES [KEY_CNT]KeyCode = getAllCodes()" >> keycodes.go
-echo "" >> keycodes.go
-echo "func getAllCodes() [KEY_CNT]KeyCode {	result := [KEY_CNT]KeyCode{};	for i := 0; i < int(KEY_CNT); i++ {	result[i] = KeyCode(i) } return result; }" >> keycodes.go
-echo "" >> keycodes.go
+#echo "" >> keycodes.go
+#echo "var ALL_CODES [KEY_CNT]KeyCode = getAllCodes()" >> keycodes.go
+#echo "" >> keycodes.go
+#echo "func getAllCodes() [KEY_CNT]KeyCode {	result := [KEY_CNT]KeyCode{};	for i := 0; i < int(KEY_CNT); i++ {	result[i] = KeyCode(i) } return result; }" >> keycodes.go
+#echo "" >> keycodes.go
 gofmt -w keycodes.go
