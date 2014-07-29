1f45b8322f44b8fd1b669f53cae76885b6cd7cc5
Raphael Estrada <galaktor@gmx.de>
2014-07-29 08:29:16 +0100

    generate keycodes from linux sources on github

--- uinput/get_keycodes.sh
+++ uinput/get_keycodes.sh
@@ -1,3 +1,15 @@
 #/bin/bash
 
-curl -s https://raw.githubusercontent.com/torvalds/linux/master/include/uapi/linux/input.h | grep -e KEY_ | awk '{printf("    %-21s = C.%-23s  /* %-5s */\n", $2, $2, $3)}' 
+cat ../COPYRIGHT > keycodes.go
+echo "" >> keycodes.go
+echo "package uinput" >> keycodes.go
+echo "" >> keycodes.go
+echo "/*" >> keycodes.go
+echo "  #include <linux/input.h>" >> keycodes.go
+echo "*/" >> keycodes.go
+echo "import \"C\"" >> keycodes.go
+echo "" >> keycodes.go
+echo "const(" >> keycodes.go
+curl -s https://raw.githubusercontent.com/torvalds/linux/master/include/uapi/linux/input.h | grep -e KEY_ | awk '{printf("    %-21s = C.%-23s  /* %-5s */\n", $2, $2, $3)}' >> keycodes.go
+echo ")" >> keycodes.go
+gofmt -w keycodes.go
