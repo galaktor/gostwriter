7111d4ee7ed4c52532879eaf85385436a12dbec2
Raphael Estrada <galaktor@gmx.de>
2014-07-30 08:17:35 +0100

    use template and find/replace token for extracted keycodes

--- uinput/get_keycodes.sh
+++ uinput/get_keycodes.sh
@@ -8,25 +8,10 @@ then
 fi
 
 echo "using KEYS from input.h at: $inputh"
-
-cat ../COPYRIGHT > keycodes.go
-echo "" >> keycodes.go
-echo "package uinput" >> keycodes.go
-echo "" >> keycodes.go
-echo "/*" >> keycodes.go
-echo "  #include <linux/input.h>" >> keycodes.go
-echo "*/" >> keycodes.go
-echo "import \"C\"" >> keycodes.go
-echo "" >> keycodes.go
-echo "" >> keycodes.go
-echo "type KeyCode C.__u16" >> keycodes.go
-echo "" >> keycodes.go
-echo "const(" >> keycodes.go
-cat $inputh | grep -e KEY_ | awk '{printf("    %-21s = KeyCode(C.%-23s)  /* %-5s */\n", $2, $2, $3)}' >> keycodes.go
-echo ")" >> keycodes.go
-#echo "" >> keycodes.go
-#echo "var ALL_CODES [KEY_CNT]KeyCode = getAllCodes()" >> keycodes.go
-#echo "" >> keycodes.go
-#echo "func getAllCodes() [KEY_CNT]KeyCode {	result := [KEY_CNT]KeyCode{};	for i := 0; i < int(KEY_CNT); i++ {	result[i] = KeyCode(i) } return result; }" >> keycodes.go
-#echo "" >> keycodes.go
+rm ./keycodes.go
+cp ./keycodes.go.template ./keycodes.go
+codes=$(cat $inputh | grep -e KEY_ | awk '{printf("    %-21s \\\= KeyCode\\\(C\\\.%-23s\\\)  \\\/\\\* %-5s \\\*\\\/\\n", $2, $2, $3)}')  # have to escape it all for sed...madness
+#echo "$codes"
+# WHY WON'T SED ACCEPT WHAT IS IN $CODES??
+sed -i "s/\/\*KEYCODES\*\//$codes/" keycodes.go
 gofmt -w keycodes.go
