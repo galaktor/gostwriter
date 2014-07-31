e10eb450d47f22a2ec58df76ef350873dfd836ca
Raphael Estrada <galaktor@gmx.de>
2014-07-31 21:12:25 +0100

    rename keycodes.go to be codes.go, as it's already in key namespace

--- key/get_keycodes.sh
+++ key/get_keycodes.sh
@@ -8,10 +8,10 @@ then
 fi
 
 echo "using KEYS from input.h at: $inputh"
-rm ./keycodes.go
-cp ./keycodes.go.template ./keycodes.go
+rm ./codes.go
+cp ./codes.template ./codes.go
 codes=$(cat $inputh | grep -e KEY_ | sed "s/KEY_//" | awk '{printf("    CODE_%-21s \\\= Code\\\(C\\\.KEY_%-23s\\\)  \\\/\\\* %-5s \\\*\\\/\\n", $2, $2, $3)}')  # have to escape it all for sed...madness
 #echo "$codes"
 # WHY WON'T SED ACCEPT WHAT IS IN $CODES??
-sed -i "s/\/\*KEYCODES\*\//$codes/" keycodes.go
-gofmt -w keycodes.go
+sed -i "s/\/\*KEYCODES\*\//$codes/" codes.go
+gofmt -w codes.go
