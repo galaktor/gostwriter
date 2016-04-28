8a066518faea67e4dffff93b7a9ee501f180c41e
Isaac Dupree <antispam@idupree.com>
2016-04-27 21:27:05 -0400

    update get_keycodes.sh for Linux >= 4.4

--- key/get_keycodes_kerneltip.sh
+++ key/get_keycodes_kerneltip.sh
@@ -5,5 +5,5 @@
 #  Project home:  <https://github.com/galaktor/gostwriter>
 #  Licensed under The GPL v3 License (see README and LICENSE files)
 
-curl https://raw.githubusercontent.com/torvalds/linux/master/include/uapi/linux/input.h > /tmp/input.h
-./get_keycodes.sh /tmp/input.h
+curl https://raw.githubusercontent.com/torvalds/linux/master/include/uapi/linux/input-event-codes.h > /tmp/input-event-codes.h
+./get_keycodes.sh /tmp/input-event-codes.h
